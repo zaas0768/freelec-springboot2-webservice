535235682fd45d9996675842453ddaf571316051
zaas <zaas@ubi-plus.com>
2023-10-12 10:27:35 +0900

    -- 2023.10.12 -- Lombok기능 명칭 변경

--- src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -6,7 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class HelloResponseDtoTest {
 
     @Test
-    public void 롬복_기능_테스트() {
+    public void  lombokFuntionTest() {
         //given
         String name = "test";
         int amount = 1000;
